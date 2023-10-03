3987f690cd2b44b767e1dea842bca10234bd66b6
Kondor Zita <zitakondor@gmail.com>
2023-10-03 19:35:33 +0200

    add menu alert to explain language and theme menu

--- src/app/components/settings/menu/menu.component.ts
+++ src/app/components/settings/menu/menu.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
              selector: 'app-menu',
              templateUrl: './menu.component.html',
              styleUrls: ['./menu.component.scss'],
            })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  firstTimeVisitor: boolean = true;
+  firstStepShown: boolean = false;
+
+  public ngOnInit(): void {
+    if (localStorage && localStorage.getItem('tutorial')) {
+      this.firstTimeVisitor = false;
+    }
+    console.log("First time visitor: " + this.firstTimeVisitor);
+  }
+
+  continueTutorial(): void {
+    this.firstStepShown = true;
+  }
+
+  finishTutorial(): void {
+    localStorage.setItem('tutorial', 'done');
+    this.firstTimeVisitor = false;
+  }
 
 }
