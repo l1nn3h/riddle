efe43c727ced702b5e600dfdf4923ae779739411
Kondor Zita <zitakondor@gmail.com>
2024-04-19 23:47:52 +0200

    fix changing tutorial video on language change, replace clue images with links, remove tutorial images

--- src/app/components/pages/welcome/welcome.component.ts
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {RiddleService} from '../../../services/riddle.service';
 import {Router} from '@angular/router';
 import {SavedGameStatsModel} from '../../../models/saved-game-stats-model';
-import {LanguageSelectorComponent} from '../../settings/language-selector/language-selector.component';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
              selector: 'app-welcome',
@@ -12,9 +12,8 @@ import {LanguageSelectorComponent} from '../../settings/language-selector/langua
 export class WelcomeComponent {
 
   savedGameStats: SavedGameStatsModel;
-  currentLanguage: string = LanguageSelectorComponent._currentLanguage;
 
-  constructor(private riddleService: RiddleService, private router: Router) {
+  constructor(private riddleService: RiddleService, private translateService: TranslocoService, private router: Router) {
     if (this.checkForSavedGame()) {
       this.savedGameStats = this.riddleService.getSavedGameStats();
     }
@@ -33,28 +32,8 @@ export class WelcomeComponent {
     return this.riddleService.checkForSavedGame();
   }
 
-  setImageSource(size: string): string {
-    const imagePath = '/assets/images/dummy/';
-    const language: string = document.getElementsByTagName('html')[0].getAttribute('lang');
-    const theme: string = document.getElementsByTagName('body')[0].classList.item(0) === 'light'
-                              ? 'dark'
-                              : 'light';
-    return imagePath + theme + size + language + '.jpg';
+  getActiveLanguage(lang: string): boolean {
+    return this.translateService.getActiveLang() === lang;
   }
 
-  getVideoSource(size: string): string {
-    const language: string = document.getElementsByTagName('html')[0].getAttribute('lang');
-    //default: large English video
-    let videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713512764/riddle_video/tutorial_lg_ENG_fyw3gi.mp4';
-
-    if (language === 'EN' && size === 'small') {
-      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713536392/riddle_video/tutorial_sm_ENG_ym5iqb.mp4';
-    } else if (language === 'HU' && size === 'small') {
-      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713548476/riddle_video/Riddle_tutorial_sm_HUN_ft0sif.mp4';
-    } else if (language === 'HU' && size === 'large') {
-      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713512764/riddle_video/tutorial_lg_ENG_fyw3gi.mp4';
-    }
-
-    return videoLink;
-  }
 }
