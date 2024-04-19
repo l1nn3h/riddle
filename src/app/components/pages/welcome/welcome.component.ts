345e4c428b4169fffdfb451f14a895341fce0c84
Kondor Zita <zitakondor@gmail.com>
2024-04-19 21:17:09 +0200

    fix shining solution, add video tutorial to welcome page, fix welcome page texts

--- src/app/components/pages/welcome/welcome.component.ts
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {RiddleService} from '../../../services/riddle.service';
 import {Router} from '@angular/router';
 import {SavedGameStatsModel} from '../../../models/saved-game-stats-model';
+import {LanguageSelectorComponent} from '../../settings/language-selector/language-selector.component';
 
 @Component({
              selector: 'app-welcome',
@@ -11,6 +12,7 @@ import {SavedGameStatsModel} from '../../../models/saved-game-stats-model';
 export class WelcomeComponent {
 
   savedGameStats: SavedGameStatsModel;
+  currentLanguage: string = LanguageSelectorComponent._currentLanguage;
 
   constructor(private riddleService: RiddleService, private router: Router) {
     if (this.checkForSavedGame()) {
@@ -38,5 +40,21 @@ export class WelcomeComponent {
                               ? 'dark'
                               : 'light';
     return imagePath + theme + size + language + '.jpg';
+  }
+
+  getVideoSource(size: string): string {
+    const language: string = document.getElementsByTagName('html')[0].getAttribute('lang');
+    //default: large English video
+    let videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713512764/riddle_video/tutorial_lg_ENG_fyw3gi.mp4';
+
+    if (language === 'EN' && size === 'small') {
+      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713536392/riddle_video/tutorial_sm_ENG_ym5iqb.mp4';
+    } else if (language === 'HU' && size === 'small') {
+      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713548476/riddle_video/Riddle_tutorial_sm_HUN_ft0sif.mp4';
+    } else if (language === 'HU' && size === 'large') {
+      videoLink = 'https://res.cloudinary.com/l1nn3h/video/upload/v1713512764/riddle_video/tutorial_lg_ENG_fyw3gi.mp4';
+    }
+
+    return videoLink;
   }
 }
